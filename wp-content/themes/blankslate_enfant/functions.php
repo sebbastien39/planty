f4005de2853215c8af01f0703d933de4e7cdb685
sebbastien39 <sebbastien39@gmail.com>
2023-05-30 11:31:05 +0200

    changement de thème twentytwentythree

--- wp-content/themes/blankslate_enfant/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent-syle', get_template_directory_uri() . '/style.css');
-}
---
+++ wp-content/themes/twentytwentythree_enfant/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Action qui permet de charger des scripts dans notre thème
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles(){
+    // Chargement du style.css du thème parent twentytwentythree
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    //chargement du css/theme.css du thème enfant twentytwentythree_enfant
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
