86d59b3e263d835933182521eb4d62da15a17a8d
sebbastien39 <sebbastien39@gmail.com>
2023-05-31 21:23:11 +0200

    Création thème enfant, 3 pages du site, css enfant, police syne, logo et couleur liens

---
+++ wp-content/themes/twentytwenty_enfant/footer.php
@@ -0,0 +1,8 @@
+<footer id="site-footer" class="header-footer-group footer-mentions">
+              <a href="#">Mentions légales</a>
+        </footer><!-- #site-footer -->
+
+      <?php wp_footer(); ?>
+
+   </body>
+</html>
