22f6ed8dd7f056a21f3c137e34c1ccf280bdb950
sebbastien39 <sebbastien39@gmail.com>
2023-05-31 11:55:10 +0200

    changement de thème pour twentytwenty et création thème enfant

--- wp-content/themes/twentytwentythree_enfant/functions.php
+++ wp-content/themes/twentytwentythree_enfant/functions.php
@@ -8,3 +8,12 @@ function theme_enqueue_styles(){
     //chargement du css/theme.css du thème enfant twentytwentythree_enfant
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+
+function montheme_supports()
+{
+    register_nav_menu('header', 'En tête du menu sb');
+    register_nav_menu('footer', 'Pied de page');
+}
+
+add_action('after_setup_theme', 'montheme_supports');
