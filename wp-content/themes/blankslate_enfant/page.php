8ff560aa242ae59e00e74c06c4a29cb7dd1ad46e
sebbastien39 <sebbastien39@gmail.com>
2023-06-15 14:49:04 +0200

    Amélioration page Commander

--- wp-content/themes/blankslate_enfant/page.php
+++ wp-content/themes/blankslate_enfant/page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-</header>
+<!--<header class="header">
+</header>-->
 <div class="entry-content" itemprop="mainContentOfPage">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
 <?php the_content(); ?>
