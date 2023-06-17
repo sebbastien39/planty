0927f96b6a1a52ca331315a8a9a1e9bd9592c1fd
sebbastien39 <sebbastien39@gmail.com>
2023-06-17 18:41:57 +0200

    Création hook admin, amélioration formulaire css

--- wp-content/themes/blankslate_enfant/functions.php
+++ wp-content/themes/blankslate_enfant/functions.php
@@ -11,16 +11,27 @@
 
 /* logo */
 
-( 'custom-logo' ); function themename_custom_logo_setup() { $defaults = array( 'height'      => 100, 'width'       => 400, 'flex-height' => true, 'flex-width'  => true, 'header-text' => array( 'site-title', 'site-description' ), ); add_theme_support( 'custom-logo', $defaults ); } add_action( 'after_setup_theme', 'themename_custom_logo_setup' ); 
+function themename_custom_logo_setup() { 
+    $defaults = array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ), );
+    add_theme_support( 'custom-logo', $defaults ); 
+} 
+ add_action( 'after_setup_theme', 'themename_custom_logo_setup' ); 
 
 
 /**
 * Add a custom link to the end of a specific menu that uses the wp_nav_menu() function
+* Ajouter un lien personnalisé à la fin d'un menu spécifique qui utilise la fonction wp_nav_menu()
 */
+
 add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
 function add_admin_link($items, $args){
-    if( $args->theme_location == 'my_menu' ){
-        $items = $items . '<li><a title="Admin" href="'. admin_url() .'">Admin</a></li>';
+    if( $args->theme_location == 'main-menu' && is_user_logged_in() ){
+        $items = $items . '<li class="menu menu-item admin"><a title="Admin" href="'. admin_url() .'">Admin</a></li>';
     }
     return $items;
 }
