e21f9e53886b02d503175f4d35a51354d107b677
sebbastien39 <sebbastien39@gmail.com>
2023-06-18 23:07:50 +0200

    Lien Admin CSS

--- wp-content/themes/blankslate_enfant/header.php
+++ wp-content/themes/blankslate_enfant/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <?php wp_head(); ?>
     </head>
 <body>
